e0175fd5571e24f3dff2e504e09ce4260aa6ad1c
pythondevpro <133487937+pythondevpro@users.noreply.github.com>
2023-05-14 01:34:27 +0600

    add task 1

---
+++ lab2/1.py
@@ -0,0 +1,35 @@
+# SyntaxError Ошибка синтаксиса
+# a = 7
+# if a == 7
+#    print('Верно!')
+
+# IndentationError Ошибка отступа
+# for i in range(10):
+# print(i)
+
+# NameError Ошибка в написании имени
+# prin('Hello!')
+
+# IndexError Ошибка при обращении к элементу по индексу 
+# my_list = [0]
+# my_list[1]
+
+# KeyError Ошибка при обращении к элементу по ключу 
+# my_dict = {'a': 1}
+# my_dict['b']
+
+# ImportError Ошибка импорта
+# from math import nothing
+
+# AttributeError Ошибка при обращении к атрибуту
+# my_str = 'my'
+# no_attr = my_str.noattr
+
+# ZeroDivisionError Ошибка из-за деления на ноль
+# 1/0
+
+# RuntimeError Ошибка времени исполнения
+# 1/0
+# my_str = 'my'
+# no_attr = my_str.noattr
+# from math import nothing
