d88fb755430f9ec82e60acd595ca0e270b0fcbf2
pythondevpro <133487937+pythondevpro@users.noreply.github.com>
2023-05-13 17:38:21 +0600

    add task 3

---
+++ lab1/3.py
@@ -0,0 +1,10 @@
+price = 50
+m = 1
+while m <=10:
+    print(f'Стоимость {m} кг -', m*price, 'тенге')
+    m += 1
+    
+# Решение задачи через for
+# price = 50
+# for i in range(1, 11):
+#    print(f'Стоимость {i} кг -', i*price, 'тенге')
